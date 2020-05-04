3cf802a64a07313b72b68c2f9504a7902af5ac19
Kelly Mears <developers@tinypixel.dev>
2020-05-03 18:19:15 -0700

    implement BudCLI component

--- src/components/BudCLI.js
+++ src/components/BudCLI.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from 'react'
-import {Box, Static, Color, Text, useStdout} from 'ink'
+import React, {useState, useEffect} from 'react'
+import {Box, Static, Color, Text} from 'ink'
 import useStdoutDimensions from 'ink-use-stdout-dimensions'
 import Divider from 'ink-divider'
 import Link from 'ink-link'
+import {prompt} from 'enquirer'
+import {bud as BudCore} from './../bud'
 
 /**
  * Bud CLI
@@ -10,68 +12,96 @@ import Link from 'ink-link'
  * @prop {array} children
  * @prop {bool}  final
  */
-const BudCLI = ({title = ''}) => {
+const BudCLI = ({title = '', budFile, outDir}) => {
+  const [prompts, setPrompts] = useState(null)
+  const [data, setData] = useState(null)
+  const [message, setMessage] = useState(null)
   const [appDimensions, setAppDimensions] = useState({})
-  const [globalPadding] = useState(1)
   const [stdOutColumns, stdOutRows] = useStdoutDimensions()
 
   useEffect(() => {
-    const offset = globalPadding * 2
+    const offset = 2
     setAppDimensions({
       width: stdOutColumns - offset,
       height: stdOutRows,
     })
   }, [stdOutColumns, stdOutRows])
 
-  useStdout(stdout => {
-    stdout.clearLine()
-  }, [])
+  useEffect(() => {
+    const {prompts: budFilePrompts} = require(budFile)
+    budFilePrompts && setPrompts(budFilePrompts)
+  }, [budFile])
+
+  useEffect(() => {
+    ;(async () => {
+      if (!prompts || !setPrompts) return
+
+      prompt(prompts).then(answers => {
+        setPrompts(null)
+        setData(answers)
+      })
+    })()
+  })
+
+  useEffect(() => {
+    ;(async () => {
+      if (!data) return
+
+      await BudCore.init({data, budFile, outDir})
+        .actions()
+        .subscribe({
+          next: message => setMessage(message),
+          complete: message => setMessage(message),
+        })
+    })()
+  }, [data])
 
   return (
-    <Box
-      flexDirection="column"
-      justifyContent="flex-start"
-      paddingTop={globalPadding}
-      paddingBottom={0}
-      paddingLeft={globalPadding}
-      paddingRight={globalPadding}
-      width={appDimensions.width}>
-      <ViewMast title={title} width={appDimensions.width} />
-    </Box>
+    <>
+      <ViewMast title={title} appDimensions={appDimensions} />
+      <Box flexDirection="column" justifyContent="flex-start">
+        {message && (
+          <Box marginTop={1} marginBottom={1}>
+            <Text>{message}</Text>
+          </Box>
+        )}
+      </Box>
+    </>
   )
 }
 
 /**
  * Application Mast
  */
-const ViewMast = ({width, title}) => (
-  <Static flexDirection="column" marginTop={1}>
-    <Box flexDirection="row">
-      <Box>{`🌱`}</Box>
-      <Box marginLeft={1}>
-        <Text bold>
-          <Link url="https://roots.io/bud">
-            <Color green>Bud</Color>
-          </Link>
-        </Text>
+const ViewMast = ({title, appDimensions}) => (
+  <Static width={appDimensions.width}>
+    <Box flexDirection="column">
+      <Box
+        width={50}
+        marginTop={1}
+        flexDirection="row"
+        justifyContent="space-between">
+        {title && (
+          <Box>
+            <Text>{title}</Text>
+          </Box>
+        )}
+        <Box flexDirection="row">
+          <Box>
+            <Text>{`🌱`}</Text>
+          </Box>
+          <Box marginLeft={1}>
+            <Text bold>
+              <Link url="https://roots.io/bud">
+                <Color green>{'Bud'}</Color>
+              </Link>
+            </Text>
+          </Box>
+        </Box>
       </Box>
+      <Divider padding={0} />
     </Box>
-    <Box marginTop={1}>{title}</Box>
-    <ViewDivider width={width} />
   </Static>
-)
-
-export {ViewMast}
-
-/**
- * Application Divider
- *
- * @prop {number} width
- */
-const ViewDivider = ({width}) => (
-  <Box width={width} marginTop={1} marginBottom={1}>
-    <Divider padding={0} width={width} />
-  </Box>
 )
 
 export default BudCLI
