4ab5c7243017f237a877e0f0ecc211102c8e3cdc
Kelly Mears <developers@tinypixel.dev>
2020-05-09 22:21:17 -0700

    php: php-di framework

--- src/budfiles/init/init.bud.js
+++ src/budfiles/init/init.bud.js
@@ -72,7 +72,7 @@ module.exports = {
           (await bud.fs.exists(`${wordPress}/index.php`)) &&
           (await bud.fs.exists(`${wordPress}/wp-config.php`))
 
-        ! isWordPress
+        !isWordPress
           ? observer.next('WordPress not detected.. moving on.')
           : (async () => {
               /** Backup index.php */
