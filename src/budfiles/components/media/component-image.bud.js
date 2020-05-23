aba4d6f47c3c73999a9ad361b18066ca76003af1
Kelly Mears <developers@tinypixel.dev>
2020-05-22 21:24:20 -0700

    [Improve] media component generator (#38)

---
+++ src/budfiles/components/media/component-image.bud.js
@@ -0,0 +1,15 @@
+/**
+ * Generator: Image Component
+ */
+module.exports = {
+  name: 'component-image',
+  description: 'Generate Image Component',
+  actions: [
+    {
+      action: 'template',
+      template: 'Image.js.hbs',
+      path: 'src/components/Image.js',
+      parser: 'babel',
+    },
+  ],
+}
